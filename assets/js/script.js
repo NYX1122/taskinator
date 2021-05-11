7c2c67d1bf31585f276055b01c25d0fecedb4e7a
BoDee <nyx0hemir@gmail.com>
2021-05-11 17:05:18 -0600

    add ability to add task

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+// Function for when a new task needs to be added to the to do list.
+var createTaskHandler = function() {
+    // creates a DOM object and assigns it to a newly created li element.
+    var listItemEl = document.createElement("li");
+    // assigns the class task-item to the DOM object listItemEl.
+    listItemEl.className = "task-item";
+    // assigns a text string to the DOM object listItemEl.
+    listItemEl.textContent = "This is a new task";
+    // appends a child element to the DOM object tasksToDoEl, which
+    // references the element ul in html, by passing the DOM object 
+    // listItemEl through the appendChild method.
+    tasksToDoEl.appendChild(listItemEl);
+}
+
+// Adds an event listener to the DOM object buttonEl that references
+// the only button on the webpage in the html. This event listener
+// waits for an event, in this case "click", and then performs a 
+// function in response. createTaskHandler was used here as a callback
+// function.
+buttonEl.addEventListener("click", createTaskHandler);
