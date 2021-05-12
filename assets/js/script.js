a73d850af7d78293ee1386d51fcf1b8cde95055a
BoDee <nyx0hemir@gmail.com>
2021-05-11 18:54:58 -0600

    code refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,23 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-// Function for when a new task needs to be added to the to do list.
-var createTaskHandler = function(event) {
-    // prevents the browser from running default software on form submition.
+var taskFormHandler = function(event) {
     event.preventDefault();
-    // creates a variable that is assigned the value of a specific input element's input.
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
-    console.log(taskTypeInput);
-    // creates a DOM object and assigns it to a newly created li element.
+
+    // package up data as an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    // sent it as an argument to createTaskEl
+    createTaskEl(taskDataObj);
+}
+
+var createTaskEl = function(taskDataObj) {
+    // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     
@@ -18,7 +26,7 @@ var createTaskHandler = function(event) {
     // give it a class name
     taskInfoEl.className = "task-info";
     // add HTML content to div
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
     
     listItemEl.appendChild(taskInfoEl);
     
@@ -26,9 +34,4 @@ var createTaskHandler = function(event) {
     tasksToDoEl.appendChild(listItemEl);
 }
 
-// Adds an event listener to the DOM object buttonEl that references
-// the only button on the webpage in the html. This event listener
-// waits for an event, in this case "click", and then performs a 
-// function in response. createTaskHandler was used here as a callback
-// function.
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
